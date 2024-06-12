030eadaef34487962e81c75a39402210c1a8bc79
Zack Tandy <ztandy@ualberta.ca>
2024-06-12 15:24:51 -0600

    Commit from RStudio. June 12, 2024 - ZT

--- Test_R_file.R
+++ Test_R_file.R
@@ -1,2 +1,8 @@
 #This is a test R file, created and edited via RStudio desktop.
 #Always pull before you push
+
+#use this to create new repository
+usethis::create_from_github(
+  "https://github.com/ZedEhSeeKay/HTTPS_link_from_repo",
+  destdir = "E:/R_git_rep"
+)
